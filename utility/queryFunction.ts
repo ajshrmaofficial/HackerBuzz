6d72f85730f3fd0079cdbbc9e29ce04f976eb2b2
Ajay Sharma <ajshrmaofficial@gmail.com>
2025-01-17 23:20:57 +0530

    refactor queryFunction: comment out unused code for cleaner utility file

--- utility/queryFunction.ts
+++ utility/queryFunction.ts
@@ -1,34 +1,9 @@
-import axios from "axios"
-import { HN_ITEM_TYPE } from "./definitions";
-import { HackerNewsDatabaseRef } from "./HN_Firebase";
+// import axios from "axios"
+// import { HN_ITEM_TYPE } from "./definitions";
+// import { HackerNewsDatabaseRef } from "./HN_Firebase";
 
-const HackerNews_BASE_URL = 'https://hacker-news.firebaseio.com/'
+// const HackerNews_BASE_URL = 'https://hacker-news.firebaseio.com/'
 
-const axiosClient = axios.create({
-    baseURL: HackerNews_BASE_URL,
-})
-
-// export async function fetchBestStoryIds(){
-//     const response = await axiosClient.get('/v0/beststories.json');
-//     if(response.status === 200){
-//         return response.data;
-//     }
-// }
-
-// export async function fetchItemById(id: number){
-//     const response = await axiosClient.get(`/v0/item/${id}.json`);
-//     if(response.status === 200){
-//         return response.data;
-//     }
-// }
-
-// export const fetchItemsByIds = async (indexArray: number[], lastLoadedIndex: number, threshold: number) => {
-//     const newStories = await Promise.all(
-//         indexArray.slice(lastLoadedIndex - threshold, lastLoadedIndex).map(fetchItemById)
-//     );
-//     return newStories;
-// }
-
-// export async function fetchBestStoryIds(){
-//     const response = await HackerNewsDatabaseRef.ref('v0/beststories').once('value');
-// }
+// const axiosClient = axios.create({
+//     baseURL: HackerNews_BASE_URL,
+// })
