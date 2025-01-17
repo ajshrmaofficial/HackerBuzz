12eec82b4c5cccb2300e0ed96ae5308a127985c5
Ajay Sharma <ajshrmaofficial@gmail.com>
2025-01-17 23:20:10 +0530

    refactor Loader component: move styles inside the component and update background color to match theme

--- components/loader.tsx
+++ components/loader.tsx
@@ -3,16 +3,15 @@ import { useTheme } from "@/theme/context";
 
 export default function Loader() {
     const { colors } = useTheme();
+    const styles = StyleSheet.create({
+        container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: colors.primary
+        }
+    });
     return <View style={styles.container}>
         <ActivityIndicator size='large' color={colors.text} />
     </View>
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
-    }
-});
-
