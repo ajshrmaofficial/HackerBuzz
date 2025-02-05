0f224e5060970d71e1930feef554166cf1b6287b
Ajay Sharma <ajshrmaofficial@gmail.com>
2025-02-05 14:33:09 +0530

    feat: implement swipeable actions for stories in StoriesList component

--- components/storiesList.tsx
+++ components/storiesList.tsx
@@ -1,17 +1,95 @@
 import { HN_ITEM_TYPE } from "@/utility/definitions";
-import { useEffect, useState, useCallback } from "react";
+import { useEffect, useState, useCallback, useRef } from "react";
 import StoryTile from "./storyTile";
 import { fetchItemsByIdsQuery } from "@/utility/HN_Firebase";
 import { useQuery } from "@tanstack/react-query";
-import Animated, { FadeOut, LinearTransition } from "react-native-reanimated";
+import Animated, { FadeOut, interpolate, LinearTransition, SharedValue, useAnimatedStyle } from "react-native-reanimated";
 import { useTheme } from "@/theme/context";
+import { View } from "react-native";
+import { FlashList, FlashListProps, ListRenderItemInfo } from "@shopify/flash-list";
+import Swipeable, { SwipeableMethods } from 'react-native-gesture-handler/ReanimatedSwipeable';
+import { Feather } from "@expo/vector-icons";
+import * as WebBrowser from "expo-web-browser";
+import { useBookmarks } from "@/utility/bookmarkContext";
 
 const STORY_FETCH_LIMIT = 15;
+
+const AnimatedFlashList = Animated.createAnimatedComponent<FlashListProps<HN_ITEM_TYPE>>(FlashList);
+
+const openInBrowser = async (url: string | undefined) => {
+    if (url) await WebBrowser.openBrowserAsync(url);
+  };
 
 export default function StoriesList({ postIds, currentSelected }: { postIds: string[], currentSelected: string }) {
     const [lastLoadedIndex, setLastLoadedIndex] = useState(STORY_FETCH_LIMIT);
     const [storiesData, setStoriesData] = useState<HN_ITEM_TYPE[]>([]);
     const { colors } = useTheme();
+    const {addBookmark} = useBookmarks();
+    const swipeableRefs = useRef<{ [key: string]: SwipeableMethods }>({});
+
+    const renderLeftActions = (progress: SharedValue<number>) => {
+        const animatedStyle = useAnimatedStyle(() => {
+            const opacity = interpolate(
+                progress.value,
+                [0, 0.5, 1],
+                [0, 0.5, 1]
+            );
+            return { 
+                opacity,
+            };
+        });
+        return (
+            <Animated.View style={[{ 
+                backgroundColor: colors.primary, 
+                justifyContent: 'center', 
+                alignItems: 'center', 
+                width: 100,
+                borderLeftWidth: 1,
+                borderColor: colors.border
+            }, animatedStyle]}>
+                <Feather name="globe" size={24} color={colors.text} />
+            </Animated.View>
+        );
+    }
+    
+    const renderRightActions = (progress: SharedValue<number>) => {
+        const animatedStyle = useAnimatedStyle(() => {
+            const opacity = interpolate(
+                progress.value,
+                [0, 0.5, 1],
+                [0, 0.5, 1]
+            );
+            return { 
+                opacity,
+            };
+        });
+        return (
+            <Animated.View style={[{ 
+                backgroundColor: colors.primary, 
+                justifyContent: 'center', 
+                alignItems: 'center', 
+                width: 100,
+                borderRightWidth: 1,
+                borderColor: colors.border,
+            }, animatedStyle]}>
+                <Feather name="bookmark" size={24} color={colors.text} />
+            </Animated.View>
+        );
+    }
+    
+    const swipeFn = async (direction: string, story: HN_ITEM_TYPE) => {
+        if (direction === 'left') {
+            await openInBrowser(story?.url);
+        } else {
+            addBookmark(story);
+        } 
+        
+        // Close the specific swipeable
+        const ref = swipeableRefs.current[`swipeable-${story.id}`];
+        if (ref) {
+            ref.close();
+        }
+    }
 
     const { data: newStories, isLoading } = useQuery({
         queryKey: ['stories', postIds.map(Number), lastLoadedIndex, STORY_FETCH_LIMIT],
@@ -35,22 +113,51 @@ export default function StoriesList({ postIds, currentSelected }: { postIds: str
         }
     }, [isLoading]);
 
-    const renderItem = useCallback(({ item }: { item: HN_ITEM_TYPE }) => <StoryTile story={item} />, []);
+    const renderItem = ({ item }: ListRenderItemInfo<HN_ITEM_TYPE>) => (
+        <Swipeable 
+            ref={(ref) => {
+                if (ref) {
+                    swipeableRefs.current[`swipeable-${item.id}`] = ref;
+                }
+            }}
+            friction={2}
+            leftThreshold={80}
+            rightThreshold={80}
+            renderLeftActions={renderLeftActions}
+            renderRightActions={renderRightActions}
+            onSwipeableWillOpen={(direction: string) => swipeFn(direction, item)}
+            overshootLeft={false}
+            overshootRight={false}
+            enableTrackpadTwoFingerGesture
+        >
+            <StoryTile story={item} />
+        </Swipeable>
+    )
+
+    // Update cleanup effect
+    useEffect(() => {
+        return () => {
+            Object.values(swipeableRefs.current).forEach(ref => {
+                ref?.close();
+            });
+            swipeableRefs.current = {};
+        };
+    }, []);
 
     return (
-        <Animated.View style={{ height: '100%', backgroundColor: colors.primary }} layout={LinearTransition.springify()}>
-            <Animated.FlatList
+        <View style={{ height: '100%', backgroundColor: colors.primary }}>
+            <AnimatedFlashList
                 data={storiesData}
                 renderItem={renderItem}
                 keyExtractor={(item) => `story-${item.id}`} 
                 onEndReached={handleEndReached}
                 onEndReachedThreshold={0.7}
-                style={{backgroundColor: colors.primary}}
-                exiting={FadeOut}
+                contentContainerStyle={{backgroundColor: colors.primary}}
+                estimatedItemSize={100}
+                // exiting={FadeOut}
                 layout={LinearTransition.springify()}
-                itemLayoutAnimation={LinearTransition.springify()}
             />
-        </Animated.View>
+        </View>
     );
 }
 
